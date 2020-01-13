54db8365fd42ec65eda6476dea2bcc351dd12803
Matti Mäki-Kihniä <matti.makikihnigmail.com>
2020-01-13 19:16:28 +0200

    Added more tests to parser

--- app/src/test/java/com/github/harmittaa/rateconversion/networking/RatesParserTest.kt
+++ app/src/test/java/com/github/harmittaa/rateconversion/networking/RatesParserTest.kt
@@ -1,11 +1,13 @@
 package com.github.harmittaa.rateconversion.networking
 
 import com.google.gson.JsonObject
-import org.junit.Assert.assertEquals
+import org.json.JSONObject
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import java.lang.IllegalArgumentException
 
 @RunWith(JUnit4::class)
 class RatesParserTest {
@@ -18,40 +20,77 @@ class RatesParserTest {
 
     @Test
     fun `test response is converted to a list with base rate as the first item`() {
-        val jsonObject = JsonObject()
-        val bgnProp = JsonObject()
-        jsonObject.addProperty("base", "EUR")
-        jsonObject.addProperty("date", "2018-09-06")
-        bgnProp.addProperty("BGN", 1.2076)
-        jsonObject.add("rates", bgnProp)
-
-        val response = parser.deserialize(jsonObject, null, null)
+        // given
+        val rootObject = addCurrencyProperty(createBaseRate("EUR"), "BGN")
+        // when
+        val response = parser.deserialize(rootObject, null, null)
+        // then
         assertEquals("EUR", response.base)
         assertEquals(2, response.rates.size)
     }
 
     @Test
+    fun `test deserialized response has correct conversion rate`() {
+        val expectedConversionRate = 1.234567
+        // given
+        val rootObject = addCurrencyProperty(createBaseRate("EUR"), "BGN", expectedConversionRate)
+        // when
+        val response = parser.deserialize(rootObject, null, null)
+        // then
+        assertEquals(expectedConversionRate, response.rates.first { it.code == "BGN" }.exchangeRate, 0.0)
+    }
+
+    @Test
     fun `test base rate gets stable id`() {
-        val jsonObject = JsonObject()
-        val bgnProp = JsonObject()
+        var rootObject = addCurrencyProperty(createBaseRate("EUR"), "BGN")
 
-        jsonObject.addProperty("base", "EUR")
-        jsonObject.addProperty("date", "2018-09-06")
-        bgnProp.addProperty("BGN", 1.2076)
-        jsonObject.add("rates", bgnProp)
+        val response = parser.deserialize(rootObject, null, null)
 
-        val response = parser.deserialize(jsonObject, null, null)
         assertEquals("EUR", response.base)
         assertEquals(0, response.rates.first { it.code == "EUR" }.id)
 
-        jsonObject.addProperty("base", "BGN")
-        jsonObject.addProperty("date", "2018-09-06")
-        bgnProp.addProperty("EUR", 123.45)
-        jsonObject.add("rates", bgnProp)
+        rootObject = addCurrencyProperty(createBaseRate("BGN"), "EUR")
 
-        val secondResponse = parser.deserialize(jsonObject, null, null)
+        val secondResponse = parser.deserialize(rootObject, null, null)
         assertEquals("BGN", secondResponse.base)
         assertEquals(0, secondResponse.rates.first { it.code == "EUR" }.id)
-
     }
+
+    @Test
+    fun `test id remains stable even when new rate is introduced`() {
+        var rootObject = addCurrencyProperty(createBaseRate("EUR"), "BGN")
+        val expectedBgnId = 1
+
+        val response = parser.deserialize(rootObject, null, null)
+        assertEquals(expectedBgnId, response.rates.first { it.code == "BGN" }.id)
+
+        rootObject = addCurrencyProperty(createBaseRate("EUR"), "MXN")
+
+        parser.deserialize(rootObject, null, null)
+
+        rootObject = addCurrencyProperty(createBaseRate("MXN"), "BGN")
+
+        val thirdResponse = parser.deserialize(rootObject, null, null)
+        assertEquals(expectedBgnId, thirdResponse.rates.first { it.code == "BGN" }.id)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `test when JSON object is null exception is thrown`() {
+        parser.deserialize(null, null, null)
+    }
+
+    private fun createBaseRate(baseProperty: String): JsonObject {
+        val base = JsonObject()
+        base.addProperty("base", baseProperty)
+        base.addProperty("date", "2018-09-06")
+        return base
+    }
+
+    private fun addCurrencyProperty(toObject: JsonObject, code: String, value: Double = 1.123): JsonObject {
+        val rateObject = JsonObject()
+        rateObject.addProperty(code, value)
+        toObject.add("rates", rateObject)
+        return toObject
+    }
+
 }
